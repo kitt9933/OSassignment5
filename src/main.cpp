be475cc460089120c5a13cb8e5f25e7e785b243a
Cullen <kitt9933@stthomas.edu>
2021-05-13 00:20:09 -0500

    made them clickable but its a bit of a mess right now

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,7 @@ typedef struct AppData {
     TTF_Font *font;
     //std::string currDir;
     std::vector <std::string> *filesInDir;
+    std::vector <int> fileTypeVals;
     //SDL_Texture *penguin;
     SDL_Texture *phrase;
     SDL_Texture * code;
@@ -25,14 +26,20 @@ typedef struct AppData {
     SDL_Texture * other;
     SDL_Texture * video;
 
+    
+    SDL_Rect backRect [11];
+    int shownFileTypeVals [11];
     bool phrase_selected;
-    bool pengSelected;
+    bool icon_selected;
+    SDL_Point offset;
+    //bool pengSelected;
 } AppData;
 
 void initialize(SDL_Renderer *renderer, AppData *data_ptr);
 void render(SDL_Renderer *renderer, AppData *data_ptr);
 void quit(AppData *data_ptr);
 std::vector<std::string> listDirectory(std::string path);
+int getFileType(std::string filepath);
 
 int main(int argc, char **argv)
 {
@@ -51,7 +58,11 @@ int main(int argc, char **argv)
     SDL_CreateWindowAndRenderer(WIDTH, HEIGHT, 0, &window, &renderer);
 
     std::string homeDirStr(home);
+    std::string parentDir = homeDirStr + "/..";
+    
     std::vector<std::string> currDir = listDirectory(homeDirStr);
+    currDir.push_back(parentDir);
+    
     sort(currDir.begin(),currDir.end());
 
     for(int i = 0; i < currDir.size(); i++){
@@ -72,42 +83,55 @@ int main(int argc, char **argv)
     while (event.type != SDL_QUIT)
     {
         SDL_WaitEvent(&event);
-        /*
+        
         switch(event.type)
         {
-
+            /*
             case SDL_MOUSEMOTION:
-                printf("mouse: %d, %d\n",event.motion.x, event.motion.y);
+                if (data.penguin_selected)
+                {
+                    data.penguin_rect.x = event.motion.x - data.offset.x;
+                    data.penguin_rect.y = event.motion.y - data.offset.y;
+                }
+                else if (data.phrase_selected)
+                {
+                    data.phrase_rect.x = event.motion.x - data.offset.x;
+                    data.phrase_rect.y = event.motion.y - data.offset.y;
+                }
                 break;
-                if(data.phrase_selected){
+            */
 
-                    data.phrase_location.x = event.motion.x;
-                    data.phrase_location.y = event.motion.y;
-                }
             case SDL_MOUSEBUTTONDOWN:
-                puts("Click\n");
-                if (event.button.x >= data.phrase_location.x && 
-                    event.button.x <= data.phrase_location.x + data.phrase_location.w &&
-                    event.button.y >= data.phrase_location.y &&
-                    event.button.y <= data.phrase_location.y + data.phrase_location.h;
-                )
-                {
-                    data.phrase_selected = true;
+                for(int i = 0; i < 11; i++){
+                    if (event.button.button == SDL_BUTTON_LEFT &&
+                        event.button.x >= data.backRect[i].x &&
+                        event.button.x <= data.backRect[i].x + data.backRect[i].w &&
+                        event.button.y >= data.backRect[i].y &&
+                        event.button.y <= data.backRect[i].y + data.backRect[i].h)
+                    {
+                        puts("i CLICKED");
+                        data.phrase_selected = true;
+                        data.offset.x = event.button.x - data.backRect[i].x;
+                        data.offset.y = event.button.y - data.backRect[i].y;
+
+                        //determine what file type it is
+                        int fileType = getFileType(currDir[i]);
+                        //if it is a directory go into that dir
+                        
+                        //if it is an executable run it
+
+                        break;
+                    } 
                 }
-                else if (event.button.x >= data.penguin_location.x && 
-                    event.button.x <= data.phrase_location.x + data.phrase_location.w &&
-                    event.button.y >= data.phrase_location.y &&
-                    event.button.y <= data.phrase_location.y + data.phrase_location.h;
-                )
-                {
-                    data.phrase_selected = true;
-                }
-                break;
+                
+                
+                
             case SDL_MOUSEBUTTONUP:
-                puts("unclick\n");
+                data.phrase_selected = false;
+                
                 break;
         }
-        */
+        
         //render(renderer, &data);
     }
 
@@ -160,6 +184,8 @@ void initialize(SDL_Renderer *renderer, AppData *data_ptr)
     SDL_Surface *phrase_surf = TTF_RenderText_Solid(data_ptr->font, "Hello World!", color);
     data_ptr->phrase = SDL_CreateTextureFromSurface(renderer, phrase_surf);
     SDL_FreeSurface(phrase_surf);
+
+
 }
 
 void render(SDL_Renderer *renderer, AppData *data_ptr)
@@ -171,15 +197,22 @@ void render(SDL_Renderer *renderer, AppData *data_ptr)
     // TODO: draw!
 
     //render background
-    SDL_Rect backRect;
-    backRect.x = 0;
-    backRect.y = 0;
-    backRect.w = 800;
-    backRect.h = 50;
+    //SDL_Rect backRect;
+    //data_ptr->backRect[0].x = 0;
+    //data_ptr->backRect[0].y = 0;
+    //data_ptr->backRect[0].w = 800;
+    //data_ptr->backRect[0].h = 50;
+    //SDL_Rect bg = data_ptr->backRect[0];
 
-    for (int i = 0; i < 12; i++){
 
-        SDL_RenderFillRect(renderer,&backRect);
+    SDL_Rect bg;
+    for (int i = 0; i < 11; i++){
+        data_ptr->backRect[i].x = 0;
+        data_ptr->backRect[i].y = i * 50;
+        data_ptr->backRect[i].w = 800;
+        data_ptr->backRect[i].h = 50;
+        bg = data_ptr->backRect[i];
+        SDL_RenderFillRect(renderer,&bg);
         
         if(i%2){
             SDL_SetRenderDrawColor(renderer,255,255,255,255);
@@ -188,7 +221,7 @@ void render(SDL_Renderer *renderer, AppData *data_ptr)
 
             SDL_SetRenderDrawColor(renderer,150,150,150,255);
         }
-        backRect.y +=50;
+        //bg.y +=50;
     }
 
     SDL_Rect rect;
@@ -207,14 +240,25 @@ void render(SDL_Renderer *renderer, AppData *data_ptr)
 
     int yVal = 60;
     const char* charName;
-    for (int i = 0; i < 12; i++){
+    for (int i = 0; i < 11; i++){
         rect.y = yVal;
-        SDL_RenderCopy(renderer, data_ptr->directory, NULL, &rect);
+
+        int fileType  = data_ptr->
+        if(fileType == 2){
+            SDL_RenderCopy(renderer, data_ptr->directory, NULL, &rect);
+        }
+        else{
+            SDL_RenderCopy(renderer, data_ptr->code, NULL, &rect);
+        }
         yVal = yVal + 50;
 
 
         //std::cout << "________________\n";
         std::string currFileName = data_ptr->filesInDir->at(i);
+        int nameLen = currFileName.length();
+
+        //this is to make the names look nicer
+        textRect.w = 9 * nameLen;
         charName = currFileName.c_str();
         //std::cout << currFileName << std::endl;
         
@@ -253,15 +297,32 @@ std::vector<std::string> listDirectory(std::string path){
 
     std::vector<std::string> dirContents;
 
-    
+     
 
     for (const auto & file : directory_iterator(path)){
         //std::cout << file.path() << std::endl;
         dirContents.push_back(file.path());
-        
+        /*
+        if(std::filesystem::is_directory(file.path())){
+
+            std::cout << "ITS A DIR\n";
+            std::cout << file.path() << std::endl;
+        }
+        */
     }
+   
     return dirContents;
 }
-    
 
+int getFileType(std::string filepath){
+
+    std::filesystem::path file = filepath;
+    if(std::filesystem::is_directory(file.path())){ //it is a dir
+        return 2;
+    }
+    else{
+        return 1; // it is code
+    }
+    return 5;
+}
 
